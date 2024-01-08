a6123926d50ebad5363ba81375e899b8be481181
Ian Chechin <Delnor40rus@gmail.com>
2024-01-08 15:04:43 +0300

    downloader

--- multiprocesses.py
+++ multiprocesses.py
@@ -11,3 +11,20 @@ resource = {
     "id": "####################",
     "fields": "files(name,id,webContentLink)",
 }
+
+def mydownloader( queue):
+    while True:
+        # Get the file id and name from the queue
+        item1 =  queue.get()
+        try:
+            gdown.download(item1['webContentLink'],
+                           './files/{}'.format(item1['name']),
+                           quiet=False)
+        finally:
+            queue.task_done()
+
+
+def get_files(resource):
+    res = getfilelist.GetFileList(resource)
+    files_list = res['fileList'][0]
+    return files_list
