97e09db35eca509a4a3f0aa0b4cc79019c53b2a7
Ian Chechin <Delnor40rus@gmail.com>
2024-01-08 15:02:40 +0300

    refact for threads

--- multithreads.py
+++ multithreads.py
@@ -1,9 +1,9 @@
 from queue import Queue
 from threading import Thread
 import time
-
 from getfilelistpy import getfilelist
 import gdown
+
 THREAD_POOL_SIZE = 5
 
 #update the resource object as per your API key and the gdriver folder id
