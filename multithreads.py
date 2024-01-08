75a504ee3bf22da72cf02aef694756e5aad9f33b
Ian Chechin <Delnor40rus@gmail.com>
2024-01-08 15:00:27 +0300

    08.01

--- multithreads.py
+++ multithreads.py
@@ -12,3 +12,22 @@ resource = {
     "id": "###################",
     "fields": "files(name,id,webContentLink)",
 }
+
+class DownlaodWorker(Thread):
+
+    def __init__(self, name, queue):
+        Thread.__init__(self)
+        self.name = name
+        self.queue = queue
+
+    def run(self):
+        while True:
+            # Get the file id and name from the queue
+            item1 = self.queue.get()
+            try:
+                gdown.download( item1['webContentLink'],
+                                './files/{}'.format(item1['name']),
+                                quiet=False)
+            finally:
+                self.queue.task_done()
+
